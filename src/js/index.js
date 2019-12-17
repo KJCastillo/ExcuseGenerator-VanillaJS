08a31053776003efe889bcaae051dbd2bd094aed
Kevin Castillo <kevincastillo318@gmail.com>
2019-12-17 19:53:42 +0000

    re-did original Excuse Generator on Vanilla JS to add querySelector & button

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,38 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", function() {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
 };
+
+function excuseGenerator() {
+  var who = ["The Batman ", "The Avengers ", "The Mandalorian ", "Baby Yoda "];
+  var verb = [" desintegrated ", " ate ", " confiscated ", " blew up "];
+  var what = [
+    " my laptop ",
+    " my car ",
+    " my christmas tree ",
+    "my playstation  "
+  ];
+  var when = [" yesterday!", " this morning!", " just now!", " last night!"];
+
+  var name = who[Math.floor(Math.random() * who.length)];
+  var action = verb[Math.floor(Math.random() * verb.length)];
+  var item = what[Math.floor(Math.random() * what.length)];
+  var time = when[Math.floor(Math.random() * when.length)];
+
+  var sentence =
+    "But i can't make it to class because " +
+    name +
+    action +
+    item +
+    time +
+    " Can you believe it?!";
+
+  return sentence;
+
+  //   document.querySelector("#btn").addEventListener("click", () => {
+  //     document.querySelector("#excuse").innerHTML = sentence;
+  //   });
+}
